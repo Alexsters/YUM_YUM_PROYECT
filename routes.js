24bc6eb362b36311fbb7b8589c41fa7d94f5acf4
fabiabi1234657 <147060824+fabiabi1234657@users.noreply.github.com>
2025-03-28 23:42:43 -0500

    routes.js
    ROUTES ACTUALIZADO

--- routes.js
+++ routes.js
@@ -29,6 +29,27 @@ routes.post('/', (req, res) => {
 
     });
 });
+//añadimos los dos metodos faltantes PUT y DELETE
+routes.put('/usuarios/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('UPDATE usuarios SET ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+            if (err) return res.send(err);
+            res.send('Usuario actualizado correctamente!');
+        });
+    });
+});
+
+
+routes.delete('/usuarios/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('DELETE FROM usuarios WHERE id = ?', [req.params.id], (err, rows) => {
+            if (err) return res.send(err);
+            res.send('Usuario eliminado correctamente!');
+        });
+    });
+});
 
 
 module.exports = routes
